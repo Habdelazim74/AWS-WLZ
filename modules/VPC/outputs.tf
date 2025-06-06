3941b2c3ed76e3198af474383d558096394fcf88
Habdelazim74 <hossamserry74@gmail.com>
2025-06-07 01:01:55 +0300

    Update outputs.tf

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -8,3 +8,7 @@ output "wavelength_subnet" {
   value       = aws_subnet.wavelength_subnets.id
   description = "Wavelength Subnet created."
 }
+output "route_table_id" {
+  value       = aws_route_table.vpc_routetable_wavelength.id
+  description = "Route table ID for the Wavelength subnet."
+}
