4c65287f50295f2b00503827d80581fb2f0d754b
Habdelazim74 <hossamserry74@gmail.com>
2025-06-06 04:09:49 +0300

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -79,7 +79,7 @@ resource "aws_kms_alias" "ebs-key" {
 
 #Creation of Elastic IP for Wavelength EC2
 resource "aws_eip" "wavelength_ip" {
-  network_border_group = "sn-dkr-1"
+  network_border_group = "eu-west-3-dss-wlz-1
   instance             = aws_instance.ec2_instance_wavelength.id
 
   tags = {
